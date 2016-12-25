910848362b344c68f09150fbf298aa1eb68376c9
Nicholas Tan <NicholasTan@nicholass-macbook-pro.local>
2016-12-25 13:52:16 +0800

    Create more display and link in viewController

--- Bet Simulator/ViewController.swift
+++ Bet Simulator/ViewController.swift
@@ -31,6 +31,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBAction func indexChange(_ sender: UISegmentedControl) {
     }
+    @IBOutlet weak var defaultSmallWinText: UILabel!
+    @IBOutlet weak var defaultCurrentSmallBigText: UILabel!
+    @IBOutlet weak var defaultBigWinText: UILabel!
+    @IBOutlet weak var defaultRedWinText: UILabel!
+    @IBOutlet weak var defaultCurrentRedBlackText: UILabel!
+    @IBOutlet weak var defaultBlackWinText: UILabel!
+    @IBOutlet weak var defaultEvenWinText: UILabel!
+    @IBOutlet weak var defaultCurrentEvenOddText: UILabel!
+    @IBOutlet weak var defaultOddWinText: UILabel!
+    @IBOutlet weak var smallWin: UILabel!
+    @IBOutlet weak var nextSmallBigBet: UILabel!
+    @IBOutlet weak var bigWin: UILabel!
+    @IBOutlet weak var redWin: UILabel!
+    @IBOutlet weak var nextRedBlackBet: UILabel!
+    @IBOutlet weak var blackWin: UILabel!
+    @IBOutlet weak var evenWin: UILabel!
+    @IBOutlet weak var nextEvenOddBet: UILabel!
+    @IBOutlet weak var oddWin: UILabel!
+    @IBOutlet weak var defaultSmallMinCapitalText: UILabel!
+    @IBOutlet weak var currentSmallBigWinText: UILabel!
+    @IBOutlet weak var defaultBigMinCapitalText: UILabel!
+    @IBOutlet weak var defaultRedMinCapitalText: UILabel!
+    @IBOutlet weak var currentRedBlackWinText: UILabel!
+    @IBOutlet weak var defaultBlackMinCapitalText: UILabel!
+    @IBOutlet weak var defaultEvenMinCapitalText: UILabel!
+    @IBOutlet weak var currentEvenOddWinText: UILabel!
+    @IBOutlet weak var defaultOddMinCapitalText: UILabel!
+    @IBOutlet weak var smallMinCapital: UILabel!
+    @IBOutlet weak var currentSmallBigWin: UILabel!
+    @IBOutlet weak var bigMinCapital: UILabel!
+    @IBOutlet weak var redMinCapital: UILabel!
+    @IBOutlet weak var currentRedBlackWin: UILabel!
+    @IBOutlet weak var blackMinCapital: UILabel!
+    @IBOutlet weak var evenMinCapital: UILabel!
+    @IBOutlet weak var currentEvenOddWin: UILabel!
+    @IBOutlet weak var oddMinCapital: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
