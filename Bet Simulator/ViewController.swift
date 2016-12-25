ce65a603e662027794d2e663e53c0c7a4081a70a
Nicholas Tan <NicholasTan@nicholass-macbook-pro.local>
2016-12-25 12:40:02 +0800

    Added number and basic control to link with viewController

--- Bet Simulator/ViewController.swift
+++ Bet Simulator/ViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 import AudioToolbox
 
 class ViewController: UIViewController {
-
+    @IBAction func numberTapped(_ sender: AnyObject) {
+    }
+    @IBAction func numberClear(_ sender: AnyObject) {
+    }
+    @IBAction func numberEntered(_ sender: AnyObject) {
+    }
+    @IBOutlet weak var numberDisplay: UILabel!
+    @IBOutlet weak var prevNumberDisplay: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var startValueDisplay: UILabel!
+    @IBAction func startValue(_ sender: UIStepper) {
+    }
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func indexChange(_ sender: UISegmentedControl) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.minimumValue = 5
+        stepper.stepValue = 5
+        stepper.maximumValue = 100
     }
 
     override func didReceiveMemoryWarning() {
